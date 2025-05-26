517704fbfd743c31d02c5a859e4dc8662d7fce96
Gunther Sunum <guntergarcia761@gmail.com>
2025-05-26 15:23:11 -0600

    estructura carpetas y componentes login dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//nuevas rutas
+import { LoginComponent } from './auth/login.component';
+import { DashboardComponent } from './features/dashboard.component';
+
+export const routes: Routes = [
+  {path: 'login', component : LoginComponent },
+  {path: 'dashboard', component : DashboardComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '**', redirectTo: '/login'}
+
+];
+
